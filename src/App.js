bc146e70b1bcead1eca01a3b9c37f02cfc0b94f3
Gabriel Rufino <contato@gabrielrufino.com>
2019-03-04 21:15:58 -0300

    Separando componentes entre views e components

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import {
   Switch
 } from 'react-router-dom'
 
-import Header from './Header'
-import Home from './Home'
-import About from './About'
+import Header from './components/Header'
+import Home from './views/Home'
+import About from './views/About'
 import Campaigns from './Campaigns'
-import Contact from './Contact'
+import Contact from './views/Contact'
 import Login from './Login'
 import Admin from './Admin'
-import NotFound from './NotFound'
-import Footer from './Footer'
+import NotFound from './views/NotFound'
+import Footer from './components/Footer'
 
 class App extends Component {
   render () {
